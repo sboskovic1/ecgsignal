b72e4be8e7b7caec94f27555075ac0da1de125c8
Stefan <sb121@rice.edu>
2025-04-23 14:19:13 -0500

    3/7 tests pass

--- homework5-code-released/homework5-code-released/runecg.bash
+++ homework5-code-released/homework5-code-released/runecg.bash
@@ -1,6 +1,6 @@
 clear
 javac -d bin/ -cp "lib/*" src/Examples.java src/dsl/*.java src/test/*.java src/utils/*.java src/utils/functions/*.java src/ecg/*.java src/ra/*.java src/compress/*.java
 # java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestDSL
-java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestECG
-# java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestCompress
+# java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestECG
+java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestCompress
 # java -cp "bin:lib/junit-4.13.1.jar:lib/hamcrest-all-1.3.jar" org.junit.runner.JUnitCore test.UTestRA
--- homework5-code-released/homework5-code-released/src/compress/Compress.java
+++ homework5-code-released/homework5-code-released/src/compress/Compress.java
@@ -11,46 +11,69 @@ public class Compress {
 
 	public static Query<Integer,Integer> delta() {
 		// TODO
-		return null;
+
+		return Q.pipeline(Q.emit(1, 0), Q.sWindow2((a, b) -> b - a));
 	}
 
 	public static Query<Integer,Integer> deltaInv() {
 		// TODO
-		return null;
+		return Q.pipeline(Q.emit(1, 0), Q.sWindow2((a, b) -> a + b));
 	}
 
 	public static Query<Integer,Integer> zigzag() {
 		// TODO
-		return null;
+		return Q.map((n) -> (n << 1) ^ (n >> (31)));
 
 	}
 
 	public static Query<Integer,Integer> zigzagInv() {
 		// TODO
-		return null;
+		return Q.map((n) -> (n >> 1) ^ -(n & 1));
 
 	}
 
 	public static Query<Integer,Integer> pack() {
 		// TODO
-		return null;
+        Query<Integer, Integer> builder = Q.sWindow3((a, b, c) -> (a << 2 * BLOCK_SIZE) | (b << BLOCK_SIZE) | c);
+        Query<Integer, Integer> count = Q.fold(0, (a, b) -> a + 1);
+        Query<Integer, Integer[]> compress = Q.parallel(builder, count, (a, b) -> new Integer[]{a, b});
+        Query<Integer, Integer[]> filter = Q.pipeline(compress, Q.filter(x -> x[1] % 3 == 0));
+        Query<Integer, Integer> reduce = Q.pipeline(filter, Q.map(x -> x[0]));
+		return reduce;
 
 	}
 
 	public static Query<Integer,Integer> unpack() {
 		// TODO
-		return null;
+        Query<Integer, Integer[]> expand = Q.map(x -> new Integer[]{x >> (2 * BLOCK_SIZE), (x >> BLOCK_SIZE) & ((1 << BLOCK_SIZE) - 1), x & ((1 << BLOCK_SIZE) - 1), 0});
+        Query<Integer, Integer> count = Q.fold(0, (a, b) -> a + 1);
+        Query<Integer, Integer> doubleCount = Q.pipeline(count, Q.dup());
+        Query<Integer, Integer[]> dup = Q.pipeline(expand, Q.pipeline(Q.dup(), Q.dup()));
+        Query<Integer, Integer[]> merge = Q.parallel(dup, doubleCount, (a, b) -> new Integer[]{a[0], a[1], a[2], b});
+        Query<Integer, Integer[]> filter = Q.pipeline(merge, Q.filter(x -> !(x[3] % 4 == 0)));
+        Query<Integer, Integer> reduce = Q.pipeline(filter, Q.map(x -> {
+            if (x[3] % 4 == 1) {
+                return x[0];
+            } else if (x[3] % 4 == 2) {
+                return x[1];
+            } else {
+                return x[2];
+            }
+        }));
+
+		return reduce;
 	}
 
 	public static Query<Integer,Integer> compress() {
 		// TODO
-		return null;
+		return Q.pipeline(delta(), Q.pipeline(zigzag(), pack()));
 	}
 
 	public static Query<Integer,Integer> decompress() {
 		// TODO
-		return null;
+		return Q.pipeline(unpack(), Q.pipeline(zigzagInv(), deltaInv()));
 	}
+
 
 	public static void main(String[] args) {
 		System.out.println("**********************************************");
