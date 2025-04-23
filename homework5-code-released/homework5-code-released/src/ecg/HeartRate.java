261306bf1adf2861eb473909281157f79f42c9b2
Stefan <sb121@rice.edu>
2025-04-23 11:05:20 -0500

    all tests pass

--- homework5-code-released/homework5-code-released/src/ecg/HeartRate.java
+++ homework5-code-released/homework5-code-released/src/ecg/HeartRate.java
@@ -11,34 +11,40 @@ public class HeartRate {
 
 	// RR interval length (in milliseconds)
 	public static Query<Integer,Double> qIntervals() {
-		// TODO
-		return null;
+        Query<Integer,Long> peaks = PeakDetection.qPeaks();
+        Query<Long, Double> diff = Q.sWindow2((a, b) -> (double)(b - a));
+		return Q.pipeline(peaks, diff);
 	}
 
 	// Average heart rate (over entire signal) in bpm.
 	public static Query<Integer,Double> qHeartRateAvg() {
-		// TODO
-		return null;
+		return Q.pipeline(Q.map((i) -> (double)i), Q.foldAvg());
 	}
 
 	// Standard deviation of NN interval length (over the entire signal)
 	// in milliseconds.
 	public static Query<Integer,Double> qSDNN() {
-		// TODO
-		return null;
+		return Q.pipeline(PeakDetection.qPeaks(), Q.pipeline(Q.map(i -> (double)i), Q.foldStdev()));
 	}
 
 	// RMSSD measure (over the entire signal) in milliseconds.
 	public static Query<Integer,Double> qRMSSD() {
-		// TODO
-		return null;
+		Query<Integer,Double> peaks = qIntervals();
+        Query<Integer, Double> sq = Q.pipeline(peaks, Q.map(i -> i * i));
+        Query<Integer, Double> avg = Q.pipeline(sq, Q.foldAvg());
+        Query<Integer, Double> sqrt = Q.pipeline(avg, Q.map(i -> Math.sqrt(i)));
+
+		return sqrt;
 	}
 
 	// Proportion (in %) derived by dividing NN50 by the total number
 	// of NN intervals (calculated over the entire signal).
 	public static Query<Integer,Double> qPNN50() {
-		// TODO
-		return null;
+		Query<Integer,Double> peaks = qIntervals();
+        Query<Integer, Double> map50 = Q.pipeline(peaks, Q.map(i -> i > 50 ? 1.0 : 0.0));
+        Query<Integer, Double> avg = Q.pipeline(map50, Q.foldAvg());
+        Query<Integer, Double> pnn50 = Q.pipeline(avg, Q.map(i -> i * 100));
+		return pnn50;
 	}
 
 	public static void main(String[] args) {
